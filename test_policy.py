2521c07ffabbfcfaccbf99058367cc5bcd402c41
AlexandreBrown <alexandrebrown97@gmail.com>
2024-11-12 15:45:11 -0500

    Removed inpainting

--- test_policy.py
+++ test_policy.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 fps = 30
+img_height = 256
+img_width = 256
 
 camera = cv2.VideoCapture(4)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, img_width)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, img_height)
 camera.set(cv2.CAP_PROP_FPS, fps)
 codec = cv2.VideoWriter_fourcc("M", "J", "P", "G")
 camera.set(cv2.CAP_PROP_FOURCC, codec)
@@ -14,45 +16,39 @@ if not camera.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-background_img = cv2.imread("assets/imgs/img1.jpg")
+distraction_img = cv2.imread("assets/imgs/img1.jpg")
+distraction_img = cv2.resize(distraction_img, (img_width, img_height))
 
 frames = []
-results = []
+
+lower_green = np.array([32, 25, 67])
+upper_green = np.array([90, 255, 255])
 
 while True:
     return_val, frame = camera.read()
-    print(frame.shape)
-    frames.append(frame)
 
     if not return_val:
         break
 
-    background_img = cv2.resize(background_img, (frame.shape[1], frame.shape[0]))
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    lower_green = np.array([32, 25, 67])
-    upper_green = np.array([90, 255, 255])
+    
     mask = cv2.inRange(hsv, lower_green, upper_green)
 
     mask_inv = cv2.bitwise_not(mask)
+    frame_without_mask = cv2.bitwise_and(frame, frame, mask=mask_inv)
+    distraction_img_at_mask = cv2.bitwise_and(distraction_img, distraction_img, mask=mask)
+    frame = cv2.add(frame_without_mask, distraction_img_at_mask)
 
-    not_green_stuff = cv2.bitwise_and(frame, frame, mask=mask_inv)
-    green_stuff = cv2.bitwise_and(background_img, background_img, mask=mask)
-
-    result = cv2.add(not_green_stuff, green_stuff)
-
-    results.append(result)
+    frames.append(frame)
 
     if len(frames) == 60:
         break
 camera.release()
 
 output_file = "output_video.avi"
-frame_height, frame_width, _ = frames[0].shape
-video_writer = cv2.VideoWriter(output_file, codec, fps, (frame_width, frame_height))
+video_writer = cv2.VideoWriter(output_file, codec, fps, (img_width, img_height))
 
-for frame in results:
+for frame in frames:
     video_writer.write(frame)
 
 video_writer.release()
